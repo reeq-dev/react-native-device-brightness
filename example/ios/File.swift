2ace1b373e1afac7a484bb7a84c5d41842d9ee42
Ganas Alexander <nooolie@gmail.com>
2022-11-02 15:56:05 +0200

    chore: initial commit

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  DeviceBrightnessExample
+//
+
+import Foundation
---
+++ example/react-native.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  dependencies: {
+    'react-native-device-brightness': {
+      root: path.join(__dirname, '..'),
+    },
+  },
+};
